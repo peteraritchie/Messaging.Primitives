09b8799679dd664b92a100c5a646ef00c10a1894
Peter Ritchie <peterritchie@quickenloans.com>
2015-12-10 17:08:51 -0500

    updated xmldocs

--- PRI.Messaging.Primitives/IConsumer.cs
+++ PRI.Messaging.Primitives/IConsumer.cs
@@ -1,5 +1,10 @@
 ﻿namespace PRI.Messaging.Primitives
 {
+	/// <summary>
+	/// A consumer of a message of type <typeparamref name="T"/>
+	/// Messages must implement <see cref="IMessage"/>, or an <see cref="IMessage"/>-deritive like <see cref="IEvent"/> or <see cref="ICommand"/>
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IConsumer<in T> where T:IMessage
 	{
 		void Handle(T message);
