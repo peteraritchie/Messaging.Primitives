a291c3cc33d595af2d273ea9f01c447da35d6414
Peter Ritchie <peterritchie@quickenloans.com>
2015-12-10 17:13:49 -0500

    updated xmldocs

--- PRI.Messaging.Primitives/IMessage.cs
+++ PRI.Messaging.Primitives/IMessage.cs
@@ -1,5 +1,8 @@
 namespace PRI.Messaging.Primitives
 {
+	/// <summary>
+	/// The basic message type.  Types to be used as messages should implement this, <see cref="IEvent"/> or <see cref="ICommand"/>
+	/// </summary>
 	public interface IMessage
 	{
 		string CorrelationId { get; set; }
