563261bd3316883f71e8d2ddf529f412ff76ab05
peteraritchie <peter.ritchie@outlook.com>
2017-01-10 11:40:02 -0500

    Added IAsyncConsumer, included nuspec, and spelling corrections.

---
+++ PRI.Messaging.Primitives/IAsyncConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace PRI.Messaging.Primitives
+{
+	/// <summary>
+	/// A consumer of a message of type <typeparamref name="T"/> that can handle the message asynchronously
+	/// Messages must implement <see cref="IMessage"/>, or an <see cref="IMessage"/>-derivative like <see cref="IEvent"/> or <see cref="ICommand"/>
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public interface IAsyncConsumer<in T> : IConsumer<T> where T : IMessage
+	{
+		Task HandleAsync(T message);
+	}
+}
--- PRI.Messaging.Primitives/IConsumer.cs
+++ PRI.Messaging.Primitives/IConsumer.cs
@@ -2,7 +2,7 @@
 {
 	/// <summary>
 	/// A consumer of a message of type <typeparamref name="T"/>
-	/// Messages must implement <see cref="IMessage"/>, or an <see cref="IMessage"/>-deritive like <see cref="IEvent"/> or <see cref="ICommand"/>
+	/// Messages must implement <see cref="IMessage"/>, or an <see cref="IMessage"/>-derivative like <see cref="IEvent"/> or <see cref="ICommand"/>
 	/// </summary>
 	/// <typeparam name="T"></typeparam>
 	public interface IConsumer<in T> where T:IMessage
