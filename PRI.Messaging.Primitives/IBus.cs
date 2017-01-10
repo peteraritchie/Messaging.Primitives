c17596631f371c573b87602e816af6071fb277fd
peteraritchie <peter.ritchie@outlook.com>
2017-01-10 12:12:14 -0500

    Added IAsyncConsumer, nuspec file, and some spelling corrections.

--- PRI.Messaging.Primitives/IBus.cs
+++ PRI.Messaging.Primitives/IBus.cs
@@ -3,7 +3,7 @@
 	/// <summary>
 	/// supports patterns like pipe (translator: enricher, filter), TODO: splitter, aggregator, invalid messages, publish, subscribe
 	/// </summary>
-	public interface IBus : IConsumer<IMessage>
+	public interface IBus : IAsyncConsumer<IMessage>
 	{
 		void AddTranslator<TIn, TOut>(IPipe<TIn, TOut> pipe) where TIn : IMessage where TOut : IMessage;
 		void AddHandler<TIn>(IConsumer<TIn> consumer) where TIn : IMessage;
