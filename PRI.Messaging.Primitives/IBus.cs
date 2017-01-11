9d5556802220777b8f81b955689a0bc82ceb1185
peteraritchie <peter.ritchie@outlook.com>
2017-01-10 21:58:44 -0500

    Deprecated IBus.AddHandler<T>(T consumer) for bug fix in patterns.

--- PRI.Messaging.Primitives/IBus.cs
+++ PRI.Messaging.Primitives/IBus.cs
@@ -1,4 +1,6 @@
-﻿namespace PRI.Messaging.Primitives
+﻿using System;
+
+namespace PRI.Messaging.Primitives
 {
 	/// <summary>
 	/// supports patterns like pipe (translator: enricher, filter), TODO: splitter, aggregator, invalid messages, publish, subscribe
@@ -6,7 +8,9 @@
 	public interface IBus : IAsyncConsumer<IMessage>
 	{
 		void AddTranslator<TIn, TOut>(IPipe<TIn, TOut> pipe) where TIn : IMessage where TOut : IMessage;
-		void AddHandler<TIn>(IConsumer<TIn> consumer) where TIn : IMessage;
+		object AddHandler<TIn>(IConsumer<TIn> consumer) where TIn : IMessage;
+		[Obsolete("Please use IBus.RemoveHandler<TMessage>(IConsumer<TMessage> consumer, object token) with the return value of AddHandler as the token.")]
 		void RemoveHandler<TMessage>(IConsumer<TMessage> consumer) where TMessage : IMessage;
+		void RemoveHandler<TMessage>(IConsumer<TMessage> consumer, object token) where TMessage : IMessage;
 	}
 }
