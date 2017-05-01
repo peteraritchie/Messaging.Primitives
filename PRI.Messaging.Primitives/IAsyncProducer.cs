3e1eecc1e986dc2b7d57b0f7a82eeee1c6f8782a
peteraritchie <peter.ritchie@outlook.com>
2017-05-01 12:06:05 -0400

    Removed partially-implemented async support and deprecated interfaces

---
+++ PRI.Messaging.Primitives/IAsyncProducer.cs
@@ -0,0 +1,9 @@
+﻿namespace PRI.Messaging.Primitives
+{
+#if SUPPPORT_ASYNC_CONSUMER
+	public interface IAsyncProducer<out T> where T : IMessage
+	{
+		void AttachConsumer(IAsyncConsumer<T> consumer);
+	}
+#endif
+}
--- PRI.Messaging.Primitives/IProducer.cs
+++ PRI.Messaging.Primitives/IProducer.cs
@@ -1,5 +1,11 @@
 ﻿namespace PRI.Messaging.Primitives
 {
+	/// <summary>
+	/// A producer of a message of type <typeparamref name="T"/>
+	/// Messages must implement <see cref="IMessage"/>, or an <see cref="IMessage"/>-derivative like <see cref="IEvent"/> or <see cref="ICommand"/>
+	/// As messages are produced they are sent to the attached consumer.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IProducer<out T> where T:IMessage
 	{
 		void AttachConsumer(IConsumer<T> consumer);
